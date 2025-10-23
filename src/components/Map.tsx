82eda1a4d17cfe98cf9cb9a225c43e5fd887a016
Tom Dahon <tomdahon@gmail.com>
2025-10-23 16:41:39 +0200

    ✨ Add new marker on click

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,10 +2,20 @@
 
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Polyline, useMapEvents } from 'react-leaflet';
+import L from "leaflet";
 import 'leaflet/dist/leaflet.css';
+import { Marker } from "react-leaflet";
+
+ const markerIcon = new L.Icon({
+            iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            });
 
 export default function Map() {
   const [positions, setPositions] = useState<[number, number][]>([]);
+
+   
 
   const position = [43.6, 7.0]
   // Sous-composant pour gérer les clics sur la carte
@@ -31,7 +41,10 @@ export default function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <LocationMarker />
-        {positions.length > 1 && <Polyline positions={positions} color="blue" />}
+        {positions.length > 1 && <Polyline positions={positions} color="red" />}
+        {positions.map((pos, i) => (
+        <Marker key={i} position={pos} icon={markerIcon} />
+        ))}
       </MapContainer>
     </div>
   );
